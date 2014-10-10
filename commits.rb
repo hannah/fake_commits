51795b4afefa0eb4130119ad7077ce0fcb887196
Hannah Zabarsky <h.zabarsky@gmail.com>
2014-10-09 22:15:45 -0400

    basic commented outline of what code should look like

---
+++ commits.rb
@@ -0,0 +1,12 @@
+require 'date'
+
+#to make a commit on every date, go through each day in the year
+
+#modify file
+
+####shell script
+#"git add #{file}"
+#"git commit -m #{something}" --- append date with correct syntax
+#git push origin master
+
+#do for each calender day
